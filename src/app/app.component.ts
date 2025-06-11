9570cec255f2ec77275bab50c22e1f84ecd11b54
Esihackman <mabelabenahackman602@gmail.com>
2025-06-11 16:19:34 +0000

    fixed the bugs in the add to cart component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,32 @@
 import { Component } from '@angular/core';
-import desseretData from '../../public/data.json';
+import dessertData from '../../public/data.json'; 
 import { AddToCartComponent } from "./components/add-to-cart/add-to-cart.component";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-});
-
-// interface
+// interfaces
 interface Dessert {
   image: DessertImages;
   name: string;
   category: string;
   price: number;
-};
+}
 
 interface DessertImages {
   thumbnail: string;
   mobile: string;
   tablet: string;
   desktop: string;
-};
+}
 
+@Component({
+  selector: 'app-root',
+  imports: [AddToCartComponent],
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'] // <-- fixed plural
+})
 export class AppComponent {
   title = 'Product list';
-  desserts:Dessert[] | null = null;
+  desserts: Dessert[] = dessertData as Dessert[];
 
-  constructor() {
-    this.desserts = desseretData;
-  };
-};
+  constructor() { }
+}
--- src/app/components/add-to-cart/add-to-cart.component.ts
+++ src/app/components/add-to-cart/add-to-cart.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-to-cart',
+  standalone: true,
+  imports: [],
   templateUrl: './add-to-cart.component.html',
   styleUrl: './add-to-cart.component.scss'
-});
+})
 
 export class AddToCartComponent {
   isAddedToCart = false;
