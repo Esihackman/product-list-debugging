a7264c0c26ad32c0dfc4d00c27035f0c32d160c5
Esihackman <mabelabenahackman602@gmail.com>
2025-06-12 16:18:26 +0000

    worked on the cart item

---
+++ src/app/models/dessert.ts
@@ -0,0 +1,20 @@
+// interface
+export interface Dessert {
+  image: DessertImages;
+  name: string;
+  category: string;
+  price: number;
+}
+
+export interface DessertImages {
+  thumbnail: string;
+  mobile: string;
+  tablet: string;
+  desktop: string;
+}
+
+export interface CartItem {
+  name: string;
+  qty: number;
+  price: number;
+}
